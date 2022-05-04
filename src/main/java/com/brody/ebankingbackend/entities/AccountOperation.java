fad7284131147718cc9b38e1b9473a4f7baa2b92
BrodyGaudel <57298219+BrodyGaudel@users.noreply.github.com>
2022-05-04 23:12:34 +0100

    first commit: entities implementation

---
+++ src/main/java/com/brody/ebankingbackend/entities/AccountOperation.java
@@ -0,0 +1,71 @@
+package com.brody.ebankingbackend.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.brody.ebankingbackend.enums.OperationType;
+
+@Entity
+public class AccountOperation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date operationDate;
+	private double amount;
+	private OperationType type;
+	@ManyToOne
+	private BankAccount bankAccount;
+	
+	
+	
+	public AccountOperation(Long id, Date operationDate, double amount, OperationType type, BankAccount bankAccount) {
+		this.id = id;
+		this.operationDate = operationDate;
+		this.amount = amount;
+		this.type = type;
+		this.bankAccount = bankAccount;
+	}
+	public AccountOperation() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getOperationDate() {
+		return operationDate;
+	}
+	public void setOperationDate(Date operationDate) {
+		this.operationDate = operationDate;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public OperationType getType() {
+		return type;
+	}
+	public void setType(OperationType type) {
+		this.type = type;
+	}
+	public BankAccount getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+	
+	
+
+}
